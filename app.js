0d146a92f5c2b8626ff3c920e6593e7a6cd25b34
gurcancekic <gurcan.cekic@reengen.com>
2022-07-05 21:24:27 +0300

    lesson 04

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import conn from './db.js';
+import pageRoute from './routes/pageRoute.js';
 
 dotenv.config();
 
@@ -17,13 +18,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 //routes
-app.get('/', (req, res) => {
-  res.render('index');
-});
-
-app.get('/about', (req, res) => {
-  res.render('about');
-});
+app.use('/', pageRoute);
 
 app.listen(port, () => {
   console.log(`Application is running on port: ${port}`);
