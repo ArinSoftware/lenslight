653fc2bcd6336b980645ea1723f02e84ad258f14
gurcancekic <arinyazilim@gmail.com>
2022-07-11 00:43:53 +0300

    lesson 13

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -23,4 +23,9 @@ const getLoginPage = (req, res) => {
   });
 };
 
-export { getIndexPage, getAboutPage, getRegisterPage, getLoginPage };
+const getLogout = (req, res) => {
+  res.cookie('jwt', '', { maxAge: 1 });
+  res.redirect('/');
+};
+
+export { getIndexPage, getAboutPage, getRegisterPage, getLoginPage, getLogout };
--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -18,4 +18,25 @@ const authenticateToken = (req, res, next) => {
   }
 };
 
-export { authenticateToken };
+const checkUser = (req, res, next) => {
+  const token = req.cookies.jwt;
+
+  if (token) {
+    jwt.verify(token, process.env.JWT_SECRET, async (err, decodedToken) => {
+      if (err) {
+        console.log(err.message);
+        res.locals.user = null;
+        next();
+      } else {
+        const user = await User.findById(decodedToken.userId);
+        res.locals.user = user;
+        next();
+      }
+    });
+  } else {
+    res.locals.user = null;
+    next();
+  }
+};
+
+export { authenticateToken, checkUser };
